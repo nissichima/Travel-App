913d605e86d0b2b46ef59db2e315e25194d24202
Nissi Chima <nissichima@Nissis-MacBook-Pro.local>
2021-07-08 22:58:25 -0400

    sass files not oading

--- src/server/index.mjs
+++ src/server/index.mjs
@@ -1,18 +1,84 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const async = require('express-async-errors')
+const fetch = require('node-fetch')
+
+/* Server Setup */
+const app = express();
+app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(express.static('./dist'));
+
+const PORT = 8081;
+app.listen(PORT, () => {
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
+
+/* Trips Array */
+
+const trips = [];
+
+/* Routes */
+
+app.get('/', (req, res) => {
+  res.status(200).send('./dist/index.html');
+});
+
+app.post('/save', (req, res, next) => {
+  if (req.body !== " ") {
+    const trip = req.body.trip;
+    trips.push(trip);
+    res.status(201).send(trip);
+  } else {
+    res.status(400).json('Bad Request');
+  }
+});
+
+app.post('/forecast', async (req, res, next) => {
+  if (req.body.endpoint !== " ") {
+    const endpoint = req.body.endpoint;
+    try {
+      const response = await fetch(endpoint);
+      if (response.ok) {
+        const jsonRes = await response.json();
+        res.status(201).send(jsonRes);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    res.status(400).json('Bad Request');
+  }
+});
+
+export { app };
+
+
+
+/*
+
 import path from 'path';
 import express from 'express';
-//import mockAPIResponse from './mockAPI.js';
+import mockAPIResponse from './mockAPI.js';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
 
-/*var path = require('path');
+var path = require('path');
 const express = require('express');
 const mockAPIResponse = require('./mockAPI.js');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fetch = require('node-fetch');*/
+const fetch = require('node-fetch');
 
 dotenv.config();
 const api_key = process.env.API_KEY;
@@ -48,4 +114,4 @@ app.post('/userText', async(req, res) => {
       }catch (error) {
       console.log("error", error);
       }
-});
+});*/
