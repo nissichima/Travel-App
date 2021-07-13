97f488f1717044b7ed43aacf758ea094e84a2ee1
Nissi Chima <nissichima@Nissis-MacBook-Pro.local>
2021-07-13 18:42:57 -0400

    removed

--- src/client/index.js
+++ src/client/index.js
@@ -50,36 +50,3 @@ export {
     formatDate
 }
 
-// IIFE function used to restrict the departure dates a user can select starting 
-// "today," or the day they use this app, to 15 days from today. This is to 
-// work around the weatherbit 16 day, starting from the respective "today," forecast limit
-//might remove?
-/*
-(function () {
-    const d = new Date();
-    const dateArrival = document.getElementById('date');
-
-    let minDay = d.getDate();
-    let minMonth = d.getMonth() + 1; 
-    let minYear = d.getFullYear();
-
-    d.setDate(d.getDate() + 15);
-    // console.log(d);
-
-    let maxDay = d.getDate();
-    let maxMonth = d.getMonth() + 1;
-    let maxYear = d.getFullYear();
-    // console.log(d.getYear());
-    // console.log(maxYear);
-
-    let minDate = formatDate(minDay, minMonth, minYear);
-    let maxDate = formatDate(maxDay, maxMonth, maxYear);
-    // console.log(minDate, maxDate);
-
-    dateDeparture.setAttribute('min', minDate);
-    dateDeparture.setAttribute('max', maxDate);
-    dateReturn.setAttribute('min', minDate);
-
-    dateDeparture.value = minDate;
-    dateReturn.value = maxDate;
-})();*/
